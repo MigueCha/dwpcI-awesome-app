4987760ef4849b88d9ab60225f41d4fd808242fb
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-11-06 03:54:22 +0000

    ⭐ Primer Middleware

--- app.js
+++ app.js
@@ -2,16 +2,25 @@
 import http from 'http';
 // Importando Express
 import express from 'express';
+
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
+
+// Registrando el primer middleware
+app.use((req, res, next)=>{
+  console.log("📢 Middleware #1");
+  // Se invoca al siguiente middlware
+  next();
+});
+
 // Asignando el middleware de express
 // al servidor http
 const server = http.createServer(app);
+
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
 // Arrancando el servidor
 server.listen(port, ip, () => {
-  console.log(`🤖 Sirviendo en http://localhost:${port}`);
-});
+  console.log(`🤖 Sirviendo en http://localhost:${port}`)});
