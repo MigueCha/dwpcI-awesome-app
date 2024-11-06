22fc09c8a80732d614c6817cc60429c737040ea6
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-11-06 05:03:56 +0000

    ⭐ Router

--- app.js
+++ app.js
@@ -1,61 +1,21 @@
-// Importando el modulo http
-import http from 'http';
 // Importando Express
 import express from 'express';
-import { log } from 'console';
+
+// Importando el enrutador
+import adminRouter from './routes/admin.route.js';
+import shopRouter from './routes/shop.route.js';
 
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
 
 // Se registra el middleware del body-parser
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// Ruta about
-// GET /about
-app.use('/about',(req, res)=>{
-  console.log("📢 Sirviendo la ruta '/about'");
-  // Se contesta al server
-  res.send(`
-    <h1>🪄 About...</h1>
-    <p>App for Fullstack Web Dev Course I!</p>
-  `);
-});
-
-// GET '/add-product'
-app.get('/add-product', (req, res, next) => {
-  // Si la petición es post pasamos el siguiente
-  // Middleware
-  if(req.method === "POST") return next();
-
-  // Servimos el formulario
-  console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
-});
-
-// POST '/add-product'
-app.post('/add-product', (req, res)=>{
-  // Realizaremos la extracción de
-  // parametros dentro de la peticion
-  console.log(req.body);
-  res.redirect('/');
-});
-
-// Ruta Raíz
-// GET /
-app.use((req, res)=>{
-  console.log("📢 Sirviendo la ruta '/'");
-  // Se contesta al server
-  res.send(`
-    <h1>Welcome to Express Js</h1>
-    <p>This is my awesome app! 😎</p>
-  `);
-});
+// Se agrega ruta de administrador
+app.use(adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
 
 // Definiendo puertos
 const port = 3000;
