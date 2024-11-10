a51621f1fb1ee1606315dc576f5e6940e260c0ab
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-11-10 22:43:10 +0000

    ⭐ Routes and Middlewares

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ import express from 'express';
 // que basicamente es un middleware
 const app = express();
 
-// Registrando el primer middleware
-app.use((req, res, next)=>{
-    console.log("📢 Middleware #1");
-    // Se invoca al siguiente middlware
-    next();
-  });
+// Ruta about
+// GET /about
+app.use('/about',(req, res)=>{
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
 
-  // Registrando el segundo middleware
-app.use((req, res, next)=>{
-    console.log("📢 Middleware #2");
-    // Se contesta al server
-    res.send(`
-      <h1>Welcome to Express Js</h1>
-      <p>This is my awesome app! 😎</p>
-    `);
-  });
-  
+// Ruta Raíz
+// GET /
+app.use((req, res)=>{
+  console.log("📢 Sirviendo la ruta '/'");
+  // Se contesta al server
+  res.send(`
+    <h1>Welcome to Express Js</h1>
+    <p>This is my awesome app! 😎</p>
+  `);
+});
+
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
