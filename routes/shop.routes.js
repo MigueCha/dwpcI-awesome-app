3b203fb13b3281250e1ad6b6d207f9c85a40a63c
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-12-03 04:52:54 +0000

    🧩 Vistas Parciales

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -14,7 +14,7 @@ import { products } from './admin.routes.js';
   // Mostrando productos en memoria
   console.log(products);
   console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop');
+  res.render('shop', {shop: 'active', docTitle:"Shop"});
 });
 
 // GET /about
