253f5982e545912f746d897f6748422bacb723cf
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-12-04 02:58:22 +0000

    🏺 Modelo Product

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const products = [];
+export default class Product {
+  constructor(title) {
+    this.title = title;
+  }
+  // Methods
+  save() {
+    products.push(this);
+  }
+  static fetchAll() {
+    return products;
+  }
+}
